94f167d1df2c52e4bf3eb76d738891fe89b55929
Aaron Westhoff <axw60080@ucmo.edu>
2019-12-01 12:50:45 -0600

    Update TeamPages.js

--- imports/ui/pages/TeamPages/TeamPages.js
+++ imports/ui/pages/TeamPages/TeamPages.js
@@ -4,7 +4,7 @@ import './TeamPages.scss';
 const TeamPages = () => (
   <div className="team-pages-page">
     <h2>Team Profiles</h2>
-    <img src="Logos/ArizonaCardinals.png" alt="Arizona Cardinals" style="width:250px;height:250px;border:0" />
+    <Link to='https://sports-social.herokuapp.com/'><img src="Logos/ArizonaCardinals.png" alt="Arizona Cardinals" style="width:250px;height:250px;border:0" /></Link>
 
 
   </div>
