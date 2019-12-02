c85a7654cde416656955033649d9176566ec0f62
Aaron Westhoff <axw60080@ucmo.edu>
2019-12-01 21:11:28 -0600

    Update TeamPages.js

--- imports/ui/pages/TeamPages/TeamPages.js
+++ imports/ui/pages/TeamPages/TeamPages.js
@@ -7,131 +7,131 @@ const TeamPages = () => (
     <h2>Team Profiles</h2>
 
       <a href="ArizonaCardinals.html">
-        <Image src="Logos/ArizonaCardinals.png" alt="Arizona Cardinals" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/ArizonaCardinals.png" alt="Arizona Cardinals" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="AtlantaFalcons.html">
-        <Image src="Logos/AtlantaFalcons.png" alt="Atlanta Falcons" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/AtlantaFalcons.png" alt="Atlanta Falcons" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="BaltimoreRavens.html">
-        <Image src="Logos/balitimoreravens.png" alt="Balitimore Ravens" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/balitimoreravens.png" alt="Balitimore Ravens" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="BuffaloBills.html">
-        <Image src="Logos/BuffaloBills.png" alt="Buffalo Bills" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/BuffaloBills.png" alt="Buffalo Bills" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="CarolinaPanthers.html">
-        <Image src="Logos/CarolinaPanthers.png" alt="Carolina Panthers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/CarolinaPanthers.png" alt="Carolina Panthers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="ChicagoBears.html">
-        <Image src="Logos/ChicagoBears.png" alt="" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/ChicagoBears.png" alt="" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="CincinnatiBengals.html">
-        <Image src="Logos/CincinnatiBengals.png" alt="Cincinnati Bengals" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/CincinnatiBengals.png" alt="Cincinnati Bengals" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="ClevlandBrowns.html">
-        <Image src="Logos/ClevelandBrowns.png" alt="ClevelandBrowns" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/ClevelandBrowns.png" alt="ClevelandBrowns" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="DallasCowboys.html">
-        <Image src="Logos/DallasCowboys.png" alt="Dallas Cowboys" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/DallasCowboys.png" alt="Dallas Cowboys" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="DenverBroncos.html">
-        <Image src="Logos/DenverBroncos.png" alt="Denver Broncos" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/DenverBroncos.png" alt="Denver Broncos" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="DetroitLions.html">
-        <Image src="Logos/DetroitLions.png" alt="Detroit Lions" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/DetroitLions.png" alt="Detroit Lions" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="GreenBayPackers.html">
-        <Image src="Logos/GreenBayPackers.png" alt="Green Bay Packers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/GreenBayPackers.png" alt="Green Bay Packers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="HoustonTexans.html">
-        <Image src="Logos/HoustonTexans.png" alt="Houston Texans" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/HoustonTexans.png" alt="Houston Texans" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="IndianapolisColts.html">
-        <Image src="Logos/IndianApolisColts.png" alt="IndianApolis Colts" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/IndianApolisColts.png" alt="IndianApolis Colts" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="JacksonvilleJaguars.html">
-        <Image src="Logos/JacksonvilleJaguars.png" alt="Jacksonville Jaguars" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/JacksonvilleJaguars.png" alt="Jacksonville Jaguars" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="KansasCityChiefs.html">
-        <Image src="Logos/KansasCityChiefs.png" alt="Kansas City Chiefs" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/KansasCityChiefs.png" alt="Kansas City Chiefs" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="LosAngelesChargers.html">
-        <Image src="Logos/LosAngelesChargers.png" alt="Los Angeles Chargers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/LosAngelesChargers.png" alt="Los Angeles Chargers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="LosAngelesRams.html">
-        <Image src="Logos/LosAngelesRams.png" alt="Los Angeles Rams" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/LosAngelesRams.png" alt="Los Angeles Rams" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="MiamiDolphins.html">
-        <Image src="Logos/MiamiDolphins.png" alt="Miami dolphins" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/MiamiDolphins.png" alt="Miami dolphins" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href=".MinnesotaVikings.html">
-        <Image src="Logos/MinnesotaVikings.png" alt="Minnesota Vikings" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/MinnesotaVikings.png" alt="Minnesota Vikings" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="NewEnglandPatriots.html">
-        <Image src="Logos/NewEnglandPatriots.png" alt="New England Patriots" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/NewEnglandPatriots.png" alt="New England Patriots" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="NewOrleansSaints.html">
-        <Image src="Logos/NewOrleansSaints.png" alt="New Orleans Saints" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/NewOrleansSaints.png" alt="New Orleans Saints" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="NewYorkGiants.html">
-        <Image src="Logos/NewYorkGiants.png" alt="New York Giants" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/NewYorkGiants.png" alt="New York Giants" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="NewYorkJets.html">
-        <Image src="Logos/NewYorkJets.png" alt="New York Jets" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/NewYorkJets.png" alt="New York Jets" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="OaklandRaiders.html">
-        <Image src="Logos/OaklandRaiders.png" alt="Oakland Raiders" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/OaklandRaiders.png" alt="Oakland Raiders" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="PhiladelphiaEagles.html">
-        <Image src="Logos/PhiladelphiaEagles.png" alt="Philadelphia Eagles" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/PhiladelphiaEagles.png" alt="Philadelphia Eagles" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="PittsburghSteelers.html">
-        <Image src="Logos/PittsburghSteelers.png" alt="PittsburghSteelers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/PittsburghSteelers.png" alt="PittsburghSteelers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="SanFrancisco49ers.html">
-        <Image src="Logos/SanFrancisco49ers.png" alt="SanFrancisco 49ers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/SanFrancisco49ers.png" alt="SanFrancisco 49ers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="SeattleSeahawks.html">
-        <Image src="Logos/SeattleSeahawks.png" alt="Seattle Seahawks" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/SeattleSeahawks.png" alt="Seattle Seahawks" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="TampaBayBuccaneers.html">
-        <Image src="Logos/TampaBayBuccaneers.png" alt="Tampa Bay Buccaneers" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/TampaBayBuccaneers.png" alt="Tampa Bay Buccaneers" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="TennesseeTitans.html">
-        <Image src="Logos/TennesseeTitans.png" alt="Tennessee Titans" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/TennesseeTitans.png" alt="Tennessee Titans" style="width:250px;height:250px;border:0"/>
       </a>
 
       <a href="WashingtonRedskins.html">
-        <Image src="Logos/WashingtonRedskins.png" alt="Washington Redskins" style="width:250px;height:250px;border:0"/>
+        <img src="Logos/WashingtonRedskins.png" alt="Washington Redskins" style="width:250px;height:250px;border:0"/>
       </a>
 
 
