69d6dd5cdedd8d372fce9ced295830e143ca0e98
Aaron Westhoff <axw60080@ucmo.edu>
2019-12-01 20:30:02 -0600

    Update TeamPages.js

--- imports/ui/pages/TeamPages/TeamPages.js
+++ imports/ui/pages/TeamPages/TeamPages.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './TeamPages.scss';
-import '../Logos';
+
 
 const TeamPages = () => (
   <div className="team-pages-page">
     <h2>Team Profiles</h2>
-      <Image src="ArizonaCardinals.png" />
+      <Image src="../Logos/ArizonaCardinals.png" />
   </div>
 );
 
